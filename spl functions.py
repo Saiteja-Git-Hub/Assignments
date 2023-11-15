1f2190d86ab0d2a1fb559bf6b5f7ebbf0f58e8fa
Saiteja <150785160+Saiteja-Git-Hub@users.noreply.github.com>
2023-11-15 12:38:18 +0530

    Add files via upload

---
+++ spl functions.py
@@ -0,0 +1,30 @@
+#UPPER#.......
+#a='this is python class'
+#b='i use upper function'
+#print(a.upper())
+#print(b.upper())
+
+#lower#........
+#A='THIS IS PYTHON CLASS'
+#B='I USE LOWER FUNCTION'
+#print(A.lower())
+#print(B.lower())
+
+#Title#..........
+#a='this is python class'
+#b='i use title function'
+#print(a.title())
+#print(b.title())
+
+#T='this is python class'
+#x=T.title()
+#print(x)
+
+#swap#..........
+#s='this is saiteja'
+#print(s.swapcase())
+
+#capital#........
+a='this is saiteja'
+print(a.capitalize())
+
