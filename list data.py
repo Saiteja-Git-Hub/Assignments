198a551557a00c90cfba5c2750f7f3c0aa9d2c7f
Saiteja <150785160+Saiteja-Git-Hub@users.noreply.github.com>
2023-11-22 13:11:18 +0530

    list data.py

--- list data.py
+++ list data.py
@@ -68,14 +68,60 @@
 #print(l)
 
 #adding multiple elements at a time
-l=[]
-for i in range(0,21):
-    l.append(i)
-print(l)
+#l=[]
+#for i in range(0,21):
+    #l.append(i)
+#print(l)
 
 #insert
 #l=[1,2,3,4,5,6,7,8,9]
 #l.insert(1,200)
+#print(l)
+
+#extend the lists
+#l1=['sai','siva','rajesh']
+#l2=[40,30,20]
+#l1.extend(l2)
+#print(l1)
+
+#l1=['sai','siva','rajesh']
+#l2=[40,30,20]
+#l2.extend(l1)
+#print(l2)
+
+#l1=['sai','siva','rajesh']
+#l2=[40,30,20]
+#l2.extend('ramesh')
+#print(l1)
+#print(l2)
+
+#remove the element in list
+#l=[10,20,30,40,'sai',50,60]
+#l.remove('sai')
+#print(l)
+
+#pop(returning the element)
+l=[10,20,30,40,'sai',50,60]
+#print(l.pop(4))
+#print(l.remove(40))
+#print(l)
+
+#reverse
+#l=[10,20,30,40,'sai',50,60]
+#l.reverse()
+#print(l)
+
+#l=[20,4,10,7,5,6]
+#l.reverse()
+#print(l)
+
+#sort
+#l=[34,4,6,9,2,1,8,3,0]
+#l.sort()
+#print(l)
+
+#l=['Sai','venu','karthik']
+#l.sort()
 #print(l)
 
 
