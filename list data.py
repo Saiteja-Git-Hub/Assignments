2d1eff27412e267543022474ff5db87a8ece0290
Saiteja <150785160+Saiteja-Git-Hub@users.noreply.github.com>
2023-11-22 09:31:19 +0530

    Add files via upload

---
+++ list data.py
@@ -0,0 +1,85 @@
+#s='saiteja'
+#l=list(s)
+#print(l)
+
+#l=[10,20,30,[40,50,60],70,80,90]
+#print(l[2],l[3][0])
+
+#l=[40,30,60,20,66,84,[90,50]]
+#print(l[5],l[6][0],l[6][1])
+
+#l=[10,20,30,[40,50,60],70,80,90]
+#print(l[4:])
+
+#replace the element in list nested
+#l=[10,20,30,[40,50,60],70,80,90]
+#l[3]=99
+#print(l)
+
+#insert the element
+#l=[10,20,30,[40,50,60],70,80,90]
+#l.insert(3,99)
+#print(l)
+
+#acess list of elements using while
+#l=[1,2,3,4,5,6,7,8,9]
+#i=0
+#while i<len(l):
+    #print(l[i])
+    #i=i+1
+
+#using for loop
+#l=[1,2,3,4,5,6,7,8,9]
+#for x in l:
+    #if x%2==0:
+      #print(x) 
+
+#finding positive and negative index from a list
+#l=['a','b','c','d','e']
+#a=len(l)
+#for i in range(a):
+    #print(l[i],'is at +ve index is',i,'and at -ve index is',i-a)
+
+#functions of the list
+
+#count# how many times it will repeat the element
+#l=[10,20,30,40,50,60,70,40]
+#print(len(l))
+#print(l.count(40))
+#print(l.count(20))
+#print(l.count(90))
+
+#index it will find first occurance of element
+#print(l.index(40))
+#print(l.index(30))
+#print(l.index(10))
+#print(l.index(700))
+
+#append adding element in top of the list or end
+#l.append(100)
+#print(l)
+#l.append(10)
+#print(l)
+
+#remove
+#l.remove(40)
+#print(l)
+#l.remove(10)
+#print(l)
+
+#adding multiple elements at a time
+l=[]
+for i in range(0,21):
+    l.append(i)
+print(l)
+
+#insert
+#l=[1,2,3,4,5,6,7,8,9)
+#l.insert([1,100,200])
+#print(l)
+
+
+
+
+
+
