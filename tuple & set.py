1b051c24646203a966fda2a4ea6aa6e38089c1c1
Saiteja <150785160+Saiteja-Git-Hub@users.noreply.github.com>
2023-11-29 14:27:14 +0530

    Add files via upload

---
+++ tuple & set.py
@@ -0,0 +1,36 @@
+#using tupple finding sum & avg
+
+#tuple=eval('eneter digists:')
+#sum_numbers=sum(tuple)
+#average_numbers=sum_numbers/len(tuple)
+#print(f'sum:{sum_numbers}')
+#print(f'average:{average_numbers}')
+
+#functions
+#pop
+#s={1,2,3,4,5,6,7}
+#s.pop()
+#print(s)
+
+#print(s.pop())
+#print(s)
+
+#s.pop()
+#s.pop()
+#print(s)
+
+#remove
+#s={10,20,30,40,50,60,70}
+#s.remove(30)
+#print(s)
+
+#print(s.remove(40))
+#print(s)
+
+
+#discard
+s={10,20,30,40,50,60,70}
+s.discard(70)
+print(s)
+
+
