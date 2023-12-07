bcec748cbc6c3bf2ae47007edd7ced9cb3741baf
Saiteja <150785160+Saiteja-Git-Hub@users.noreply.github.com>
2023-12-07 14:29:16 +0530

    Add files via upload

---
+++ dict data.py
@@ -0,0 +1,36 @@
+#adding new element and value update only
+
+#d={100:'sai',200:'siva',300:'rajesh',400:'ramesh'}
+#d[500]='vikram'
+#d[600]='hari'
+#d[700]='vinith'
+
+
+#d[100]='kiran'
+#d[400]='nag'
+#d[300]='pinky'
+#print(d)
+
+#delete element in dict
+
+#d={100:'sai',200:'siva',300:'rajesh',400:'ramesh'}
+#del d[100]
+#del d[200]
+#del d[300]
+#print(d)
+
+#clear all elements in dic
+#d={100:'sai',200:'siva',300:'rajesh',400:'ramesh'}
+#d.clear()
+
+#d[500]='balu' #after clear adding element
+#print(d)
+
+#assign the complete key to the list of elements
+l=['sai','ramesh','hari','kiran']
+#d={400:l}
+#d={500:l}
+d={600:l}
+print(d)
+
+
