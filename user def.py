a91b4a7181fa982c5be1615d34a2158e0f737ad1
Saiteja <150785160+Saiteja-Git-Hub@users.noreply.github.com>
2023-12-14 13:22:43 +0530

    Add files via upload

---
+++ user def.py
@@ -0,0 +1,129 @@
+#def sai():
+    #print('hello')
+#sai()
+#sai()
+
+#def sai(x):
+    #print('the sqrt of {} is {}'.format(x,x**0.5))
+#sai(100)
+#sai(200)
+#sai(300)
+
+#import math
+#result=math.sqrt(100)
+#print(result)
+
+#def sai(x,y):
+    #return x+y
+#print('the addition of x,y:',sai(10,20))
+
+#def sai():
+    #print('hello')
+#sai()
+#print(sai())
+
+#def sai(x):
+    #return x
+#print(sai('hello'))
+
+#def even(x):
+    #if x%2==0:
+        #print('even')
+    #else:
+        #print('odd')
+#even(2)
+#even(5)
+#even(6)
+#even(7)
+
+#def even(x):
+    #for i in x:
+        #if i%2==0:
+         #print('even')
+
+#Arguments
+#positional arg
+
+#def p(name,regno):
+    #print(name,regno)
+#p('saiteja',228)
+
+#def p(a,b):
+    #print(a+b)
+    #print(a-b)
+#p(100,200)
+#p(100,200)
+
+#def p(c,d):
+    #print(c*d)
+    #print(c/d)
+#p(4,6)
+#p(4,8)
+
+#keyword arg
+
+#def kw(a,b):
+    #print(a+b)
+#kw(a=20,b=30)
+#kw(b=30,a=20)
+
+#def kw(c,d):
+    #print(c-d)
+#kw(c=100,d=200)
+#kw(d=200,c=100)
+
+#def kw(a,b):
+    #print(a-b)
+#kw(10,b=20)
+#kw(a=10,20)#error
+
+#def kw(c,d):
+    #print(c/d)
+    #print(c/d)
+#kw(c=200,d=100)
+#kw(d=100,c=200)
+
+#default arg
+
+#def d(name,msg):
+    #print('hello',name,msg)
+#d('sai','good mrng')
+
+#def d(name='guest'):
+    #print('hello',name,'good mrng')
+#d('sai')
+#d()
+
+#def d(name='siva',reg=233):
+    #print(name,'hello',reg)
+#d()
+#d('sai')
+
+#variable length arg
+
+#def sum(*n):
+    #total=0
+    #for x in n:
+        #total=total+x
+    #print('sum:',total)
+#sum()
+#sum(10)
+#sum(10,20,30)
+#sum(10,20,30,40,50)
+
+def f(arg1,arg2,arg3=4,arg4=8):
+    print(arg1,arg2,arg3,arg4)
+f(3,2,arg3=10,arg4=20)
+
+
+
+
+
+
+
+
+
+
+
+
+
