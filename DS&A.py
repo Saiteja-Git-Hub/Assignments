7c2c65aa5c3982eae895af863416b21dabef5f2d
Saiteja <150785160+Saiteja-Git-Hub@users.noreply.github.com>
2024-02-01 15:45:03 +0530

    Add files via upload

---
+++ DS&A.py
@@ -0,0 +1,98 @@
+#stack implement using list
+#operation done in LIFO order that is one end
+#PUSH & POP operations are also done in same end
+#in list push operation as append
+
+# stack=[]
+# stack.append(10)
+# stack.append(20)
+# stack.append(30)
+# print(stack)
+
+# stack.pop()
+# stack.pop()
+# stack.pop() 
+# print(stack)
+
+#programm for stack
+
+# stack=[]
+# def push():
+#     element=int(input('Enter the element:'))
+#     stack.append(element)
+#     print(stack)
+# def pop():
+#     if not stack:
+#         print('stack is empty!')
+#     else:
+#         e=stack.pop()
+#         print('removed element',e)
+#         print(stack)
+# while True:
+#     print('select the operations: 1.push 2.pop 3.quit')
+#     choice=int(input())
+#     if choice==1:
+#         push()
+#     elif choice==2:
+#         pop()
+#     elif choice==3:
+#         break
+#     else:print('enter the correct operation!')
+
+
+#queue DS
+
+# queue=[]
+# queue.append(10) #queue.insert(0,10)
+# queue.append(20)
+# queue.append(30)
+# print(queue[0])
+# print(len(queue)==0) #print(not queue)
+# print(queue)
+
+# #queue.pop(0) #queue.pop()
+# #queue.pop(0)
+# queue.pop(0)
+# print(queue)
+
+#queue program
+
+# queue=[]
+# def enqueue():
+#     element=int(input('Eneter the elements:'))
+#     queue.append(element)
+#     print(queue)
+# def dequeue():
+#     if not queue:
+#         print('queue is empty!')
+#     else:
+#         e=queue.pop(0)
+#         print('removed element:',e)
+
+# def display():
+#     print(queue)
+
+# while True:
+#     print('select the operations: 1.push 2.pop 3.show 4.quit')
+#     choice=int(input())
+#     if choice==1:
+#         enqueue()
+#     elif choice==2:
+#         dequeue()
+#     elif choice==3:
+#         display()
+#     elif choice==4:
+#         break
+#     else:
+#         print('slect the correct operation')
+
+
+
+
+
+                
+
+
+
+
+
