24bd786deb2a85fa8d3a5c90ee3c691fbe29138b
Saiteja <150785160+Saiteja-Git-Hub@users.noreply.github.com>
2024-02-01 15:49:55 +0530

    Modules

---
+++ create.py
@@ -0,0 +1,6 @@
+x=10
+y=20
+def add(a,b):
+    print('the sum:',a+b)
+def mul(a,b):
+    print('the mul:',a*b)
---
+++ sai.py
@@ -0,0 +1,5 @@
+s=70
+def sub(a,b):
+    print('the sub is:',(a-b))
+def div(a,b):
+    print('the division:',(a/b))
